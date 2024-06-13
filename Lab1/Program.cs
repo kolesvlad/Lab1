f62bc48ce7a3f8f62b0db026b15cb838ef5fd78f
valdemar <kolesvlad@gmail.com>
2024-06-13 19:09:13 +0300

    Add: Item, Customer

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,3 +7,78 @@ class Program
         Console.WriteLine("Test passed");
     }
 }
+
+interface ICryptoSellable
+{
+    bool IsBitcoinAccepted();
+}
+
+abstract class Item : ICryptoSellable
+{
+    public long Price { get; protected init; }
+    public long Quantity { get; protected set; }
+
+    public bool IsBitcoinAccepted()
+    {
+        return true;
+    }
+
+    public string GetDisplayPriceInDollars()
+    {
+        return "$" + Price / 100 + "," + Price % 100;
+    }
+
+    public void RemoveFromStock(long quantity)
+    {
+        this.Quantity -= quantity;
+    }
+}
+
+class Smartphone : Item
+{
+    public Smartphone(long price, long quantity)
+    {
+        Price = price;
+        Quantity = quantity;
+    }
+}
+
+class Customer
+{
+    public string Name;
+    private long _availableFunds;
+
+    public Customer(string name, long availableFunds)
+    {
+        Name = name;
+        _availableFunds = availableFunds;
+    }
+
+    public void chargeMoney(long price)
+    {
+        if (IsEnoughFunds(price))
+        {
+            DeductFunds(price);
+        }
+        else
+        {
+            NullifyBalance();
+        }
+    }
+
+    public void DeductFunds(long price)
+    {
+        _availableFunds -= price;
+    }
+
+    private bool IsEnoughFunds(long price)
+    {
+        return _availableFunds > price;
+    }
+
+    private void NullifyBalance()
+    {
+        _availableFunds = 0;
+    }
+}
+
